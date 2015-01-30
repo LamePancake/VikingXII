f0ab810b548acce22648a3cdbf97dfc5f72fdb0f
Benier <timwang_98@hotmail.com>
2015-01-30 20:21:30 +0000

    Starter project

---
+++ ProjectGray/ProjectGray/GameViewController.h
@@ -0,0 +1,14 @@
+//
+//  GameViewController.h
+//  ProjectGray
+//
+//  Created by Tim Wang on 2015-01-30.
+//  Copyright (c) 2015 Tim Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <GLKit/GLKit.h>
+
+@interface GameViewController : GLKViewController
+
+@end
