098018e44054e8b26978d1bea1e79ffc4e213b1f
Benier <timwang_98@hotmail.com>
2015-01-30 20:18:00 +0000

    Initial Commit

---
+++ ProjectGray/ProjectGray/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProjectGray
+//
+//  Created by Tim Wang on 2015-01-30.
+//  Copyright (c) 2015 Tim Wang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
