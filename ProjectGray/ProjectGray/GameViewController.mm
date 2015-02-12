ec555cd64fdae790051c07027e355db6cab52bf3
Benier <timwang_98@hotmail.com>
2015-02-12 20:55:52 +0000

    Merged TimBranch into trunk

--- ProjectGray/ProjectGray/GameViewController.mm
+++ ProjectGray/ProjectGray/GameViewController.mm
@@ -9,7 +9,8 @@
 #import "GameViewController.h"
 #import <OpenGLES/ES2/glext.h>
 #import "Camera.h"
-#include <vector>
+#import "chicken.h"
+#import "chicken_triag.h"
 
 #define BUFFER_OFFSET(i) ((char *)NULL + (i))
 
@@ -86,6 +87,8 @@ GLfloat gCubeVertexData[216] =
     
     GLuint _vertexArray;
     GLuint _vertexBuffer;
+    GLuint _normalArray;
+    GLuint _normalBuffer;
     Camera *_camera;
 }
 
@@ -177,12 +180,11 @@ GLfloat gCubeVertexData[216] =
     //Load into VBO
     
   
-    glGenVertexArraysOES(1, &_vertexArray);
+    /*glGenVertexArraysOES(1, &_vertexArray);
     glBindVertexArrayOES(_vertexArray);
     
     glGenBuffers(1, &_vertexBuffer);
     glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
-    //glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
     glBufferData(GL_ARRAY_BUFFER, sizeof(gCubeVertexData), gCubeVertexData, GL_STATIC_DRAW);
     
     glEnableVertexAttribArray(GLKVertexAttribPosition);
@@ -190,8 +192,47 @@ GLfloat gCubeVertexData[216] =
     glEnableVertexAttribArray(GLKVertexAttribNormal);
     glVertexAttribPointer(GLKVertexAttribNormal, 3, GL_FLOAT, GL_FALSE, 24, BUFFER_OFFSET(12));
     
+    glBindVertexArrayOES(0);*/
+    
+    // set input data to arrays
+    glGenVertexArraysOES(1, &_vertexArray);
+    glBindVertexArrayOES(_vertexArray);
+     
+    glGenBuffers(1, &_vertexBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, _vertexBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(chicken_triagVerts), chicken_triagVerts, GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(GLKVertexAttribPosition);
+    glVertexAttribPointer(GLKVertexAttribPosition, 3, GL_FLOAT, GL_FALSE, 32, BUFFER_OFFSET(0));
+    glEnableVertexAttribArray(GLKVertexAttribNormal);
+    glVertexAttribPointer(GLKVertexAttribNormal, 3, GL_FLOAT, GL_FALSE, 32, BUFFER_OFFSET(12));
+    glEnableVertexAttribArray(GLKVertexAttribTexCoord0);
+    glVertexAttribPointer(GLKVertexAttribTexCoord0, 2, GL_FLOAT, GL_FALSE, 32, BUFFER_OFFSET(24));
+    
+    
+    //normal buffer
+    /*glGenVertexArraysOES(1, &_normalArray);
+    glBindVertexArrayOES(_normalArray);
+    
+    glGenBuffers(1, &_normalBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, _normalBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(chicken_triagNormals), chicken_triagNormals, GL_STATIC_DRAW);
+    
+    glEnableVertexAttribArray(GLKVertexAttribNormal);
+    glVertexAttribPointer(GLKVertexAttribNormal, 3, GL_FLOAT, GL_FALSE, 12, 0);*/
+    
+    
+//    glGenVertexArraysOES(1, &_normalBuffer);
+    //glEnableVertexAttribArray(GLKVertexAttribNormal);
+    //glNormalPointer(GLKVertexAttribNormal, 3, GL_FLOAT, GL_TRUE, 24, 0);
+    //glTexCoordPointer(2, GL_FLOAT, 0, chickenTexCoords);
+    
+    // draw data
+    //glDrawArrays(GL_TRIANGLES, 0, chickenNumVerts);
+    //glBindVertexArrayOES(_vertexArray);
+    //glBindVertexArrayOES(_normalArray);
     glBindVertexArrayOES(0);
-    }
+}
 
 - (void)tearDownGL
 {
@@ -255,7 +296,7 @@ GLfloat gCubeVertexData[216] =
     // Render the object with GLKit
     [self.effect prepareToDraw];
     
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glDrawArrays(GL_TRIANGLES, 0, chicken_triagNumVerts);
     
     // Render the object again with ES2
     glUseProgram(_program);
@@ -263,7 +304,7 @@ GLfloat gCubeVertexData[216] =
     glUniformMatrix4fv(uniforms[UNIFORM_MODELVIEWPROJECTION_MATRIX], 1, 0, _camera.modelViewProjectionMatrix.m);
     glUniformMatrix3fv(uniforms[UNIFORM_NORMAL_MATRIX], 1, 0, _camera.normalMatrix.m);
     
-    glDrawArrays(GL_TRIANGLES, 0, 36);
+    glDrawArrays(GL_TRIANGLES, 0, chicken_triagNumVerts);
 }
 
 #pragma mark -  OpenGL ES 2 shader compilation
