d1a548c7a17d7f08f9b2573a841c9a2d7f04c306
Shane-S <shane.d.spoor@gmail.com>
2015-03-09 05:41:47 +0000

    Continuously update task count in task manager

--- ProjectGray/ProjectGray/TaskManager.m
+++ ProjectGray/ProjectGray/TaskManager.m
@@ -32,34 +32,6 @@
     return self;
 }
 
-/*
--(void)runTasksWithDeltaTime: (CADisplayLink *)link
-{
-    _prevTime = _curTime;
-    _curTime = link.timestamp;
-    CFTimeInterval delta = _curTime - _prevTime;
-    
-    NSUInteger numTasks = [_taskList count];
-    for(NSUInteger i = 0; i < numTasks; i++)
-    {
-        id<Task> curTask = _taskList[i];
-        
-        // If there isn't a task at this slot, don't bother
-        if(!curTask) continue;
-        
-        [curTask updateWithDeltaTime: delta];
-        
-        // Replace the current task with the next one in the list (if it exists)
-        // Otherwise, add this slot to the list of empty slots
-        if(curTask.isFinished) {
-            id<Task>next = curTask.nextTask;
-
-            if(next) _taskList[i] = next;
-            else [_emptySlots addObject: [[NSNumber alloc] initWithUnsignedLong:i]];
-        }
-    }
-}*/
-
 -(void)runTasksWithCurrentTime: (NSTimeInterval) time
 {
     _prevTime = _curTime;
@@ -84,6 +56,8 @@
             if(next) _taskList[i] = next;
             else [_taskList removeObjectAtIndex: i];
         }
+        // Need to update task count; if there were multiple running at the same time and one was removed, it could go out of bounds
+        numTasks = [_taskList count];
     }
 }
 
